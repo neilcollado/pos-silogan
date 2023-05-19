3363294c8e914cc1f5152bf46711c2e861d3d51f
Dhony Mark Mendoza <19102710@usc.edu.ph>
2023-05-20 00:24:04 +0800

    added orders read and delete test

--- tests/Feature/OrderControllerTest.php
+++ tests/Feature/OrderControllerTest.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\User;
+use App\Models\Orders;
 
 class OrderControllerTest extends TestCase
 {
@@ -13,7 +14,7 @@ class OrderControllerTest extends TestCase
 
     public function test_order_create()
     {
-        $user = User::factory()->create();
+        $user = User::factory()->make();
         
         $order_info = [
             'orderNo' => 2,
@@ -30,7 +31,7 @@ class OrderControllerTest extends TestCase
 
     public function test_order_view()
     {
-        $user = User::factory()->create();
+        $user = User::factory()->make();
         
         $order_info = [
             'orderNo' => 2,
@@ -43,5 +44,46 @@ class OrderControllerTest extends TestCase
         $response = $this->call('GET', '/orders', $order_info);
 
         $response->assertStatus($response->status(), 200);
+    }
+
+    public function test_order_update(){
+        $user = User::factory()->make();
+
+        $order = [
+            'orderNo' => 2,
+            'user_id' => $user->id,
+            'emp_name' => 'Test Name',
+            'status' => 'pending',
+            'type' => 'dine-in',
+        ];
+    
+        $response = $this->put('/orders', [
+            'orderNo' => 2,
+            'user_id' => $user->id,
+            'emp_name' => 'Test Update',
+            'status' => 'completed',
+            'type' => 'dine-in',
+        ]);
+    
+        $response->assertStatus($response->status(), 200);
+    }
+
+    public function test_order_delete(){
+        $user = User::factory()->make();
+
+        $order = [
+            'id' => 4,
+            'orderNo' => 2,
+            'user_id' => $user->id,
+            'emp_name' => 'Test Name',
+            'status' => 'pending',
+            'type' => 'dine-in',
+        ];
+
+        $response = $this->delete('orders/' . $order['id']);
+
+        $response->assertStatus($response->status(), 200);
+
+        $this->assertDatabaseMissing('orders', ['id' => $order['id']]);
     }
 }
